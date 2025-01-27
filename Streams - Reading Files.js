bc739b4e9af476732e265787ed515a09575032c5
Ayushman Parchhoria <40245594+ayushmanparchoria16@users.noreply.github.com>
2025-01-27 14:42:33 +0530

    Update Streams - Reading Files.js

--- Streams - Reading Files.js
+++ Streams - Reading Files.js
@@ -1,7 +1,16 @@
-var fs = require('fs');
-var data = '';
-var readStream = fs.createReadStream('/tmp/foo.txt', { encoding: 'utf8' });
-readStream.on('data', function (chunk) {
-    data += chunk;
-    console.log(chunk);
+const fs = require('fs');
+
+// Create a read stream for data_file.txt
+const readStream = fs.createReadStream('data_file.txt', { encoding: 'utf8' });
+
+readStream.on('data', (chunk) => {
+    console.log(`Chunk size: ${chunk.length}`);
+});
+
+readStream.on('end', () => {
+    console.log('Finished reading data_file.txt');
+});
+
+readStream.on('error', (err) => {
+    console.error('Error occurred:', err);
 });
